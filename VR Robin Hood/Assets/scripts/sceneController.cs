28e62432c605ecf415d85c1a7d40162c8f5b17d6
RaphaelB2024 <rbranston2024@elam.co.uk>
2026-01-16 12:45:41 +0000

    glitchy scene

--- VR Robin Hood/Assets/scripts/sceneController.cs
+++ VR Robin Hood/Assets/scripts/sceneController.cs
@@ -13,35 +13,50 @@ public class sceneController : MonoBehaviour
     [SerializeField] public bool timeMode;
     [SerializeField] public bool targetMode;
 
+    public AudioSource badToTheBone;
+
+    private bool next = true;
+
 
     private void Update()
     {
-
+        /*
         if (timeMode)
         {
             timer -= Time.deltaTime;
-            if(timer <= 0)
+            if(timer <= 0f)
             {
                 StartCoroutine(NextScene());
             }
+        }*/
+
+        if (!badToTheBone.isPlaying && next)
+        {
+            Debug.Log("no bad to the bone. So next scene");
+            StartCoroutine(NextScene());
         }
 
         else if(targets.Length == 0 && targetMode)
         {
+            Debug.Log("Target mode next scene");
             StartCoroutine(NextScene());
         }
     }
 
     private IEnumerator NextScene()
     {
+        next = false;
         while(fadeScreenRenderer.color.a < 100)
         {
-            Color temporary = fadeScreenRenderer.GetComponent<SpriteRenderer>().color;
-            temporary.a = 1 * Time.deltaTime;
+            Debug.Log("fadeScreenRenderer.colour.a < 100");
+            Color temporary = fadeScreenRenderer.GetComponent<SpriteRenderer>().color;  
+            temporary.a += Time.deltaTime;
+            fadeScreenRenderer.color = temporary;
         }
         if(fadeScreenRenderer.color.a >= 100)
         {
-         //   SceneManager.LoadScene();
+            Debug.Log("SceneManager.LoadScene()");
+            //   SceneManager.LoadScene();
         }
 
         return null;
